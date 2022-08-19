3e76381b430d4877256e2f211be7c92d731c15f8
maivanhieu8402 <maivanhieu842002@gmail.com>
2022-08-19 20:28:18 +0700

    first commit

---
+++ program_1.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+#define pi 3.141593
+int main()
+{
+	float s;
+	printf("nhap dien tich s: ");
+	scanf("%f", &s);
+	float r = sqrt(s/(4*pi));
+	printf("the tich V: %f",(4/3*1.0)*pi*pow(r,3));
+}
